d98604d990ec621e386488eda5479319645faac2
Alberto <albertosanahuja@gmail.com>
2025-02-17 17:59:02 +0100

    actualizo vista

--- app.py
+++ app.py
@@ -350,13 +350,14 @@ def clear_sessions():
         except Exception as e:
             print("No se pudo eliminar sesiones:", e)
 
-"""
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=8080, debug=False)
-"""
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
+"""
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto
     app.run(host="0.0.0.0", port=port)
+"""
