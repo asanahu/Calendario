af3b54a2fe119a97187d615b4997f9fb61c88160
Alberto <albertosanahuja@gmail.com>
2025-02-17 13:19:47 +0100

    modifico app.py

--- app.py
+++ app.py
@@ -352,9 +352,12 @@ def clear_sessions():
 
 
 
-
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto
+    app.run(host="0.0.0.0", port=port)
+"""
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=8080, debug=False)
+"""
